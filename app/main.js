b1f8f40d4fd476e9a481740c0feebe65a2a347d4
alexanderl <alexanderl@kmslh.com>
2023-01-25 09:55:47 +0400

    Add rects

--- app/main.js
+++ app/main.js
@@ -1,2 +1,33 @@
-const canv = document.getElementById('canvas')
-const ctx = canv.getContext('2d')
+const canvasBoundingRect = canvas.getBoundingClientRect()
+canvas.width = canvasBoundingRect.width
+canvas.height = canvasBoundingRect.height
+const ctx = canvas.getContext("2d");
+
+let plx = 0
+let sign = 1
+
+function draw() {
+    const ctx = document.getElementById("canvas").getContext("2d");
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 6; j++) {
+            ctx.fillStyle = `rgb(${Math.floor(255 - plx * i)}, ${Math.floor(
+                255 - plx * j
+            )}, 0)`;
+            ctx.fillRect(j * 100, i * 100, 100, 100)
+        }
+    }
+}
+
+function march() {
+    plx += 0.1 * sign
+    if (plx > 42.5) {
+        sign = -1
+    }
+    if (plx < 0) {
+        sign = 1
+    }
+    draw();
+    setTimeout(march, 1);
+}
+
+march();
