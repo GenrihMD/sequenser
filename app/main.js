16010cf7aac97d528365ca8ea69418a9c3e30ba5
alexanderl <alexanderl@kmslh.com>
2023-01-25 18:25:22 +0400

    Move to worker

--- app/main.js
+++ app/main.js
@@ -1,33 +1,10 @@
 const canvasBoundingRect = canvas.getBoundingClientRect()
 canvas.width = canvasBoundingRect.width
 canvas.height = canvasBoundingRect.height
-const ctx = canvas.getContext("2d");
 
-let plx = 0
-let sign = 1
+const offscreen = canvas.transferControlToOffscreen();
 
-function draw() {
-    const ctx = document.getElementById("canvas").getContext("2d");
-    for (let i = 0; i < 6; i++) {
-        for (let j = 0; j < 6; j++) {
-            ctx.fillStyle = `rgb(${Math.floor(255 - plx * i)}, ${Math.floor(
-                255 - plx * j
-            )}, 0)`;
-            ctx.fillRect(j * 100, i * 100, 100, 100)
-        }
-    }
-}
+const worker = new Worker("/app/canvas-worker.js");
+worker.postMessage({ canvas: offscreen }, [offscreen]);
 
-function march() {
-    plx += 0.1 * sign
-    if (plx > 42.5) {
-        sign = -1
-    }
-    if (plx < 0) {
-        sign = 1
-    }
-    draw();
-    setTimeout(march, 1);
-}
 
-march();
